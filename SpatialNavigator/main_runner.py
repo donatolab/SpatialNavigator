220edfd69cedbb0cd4151e0cab81cc252efe713f
Nimmerzeel <72859592+Nimmerzeel@users.noreply.github.com>
2020-12-07 17:53:46 +0100

    write first set of functions

---
+++ SpatialNavigator/main_runner.py
@@ -0,0 +1,12 @@
+from simulate_activity import sim_data
+
+# simulate cell events during spatial navigation
+def sim_cell_events():
+    event_simulation = sim_data()
+    return event_simulation
+
+
+# create heatmap
+
+# convolve events into calcium traces
+
